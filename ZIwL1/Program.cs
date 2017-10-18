6d0fb23f545b56ad041eff78d3fa2c7ded9c09bb
JSzymczuk <j.szymczuk@gmail.com>
2017-10-18 22:14:45 +0200

    Dodane pobieranie danych od użytkownika

--- ZIwL1/Program.cs
+++ ZIwL1/Program.cs
@@ -10,14 +10,26 @@ namespace ZIwL1
     {
         static void Main(string[] args)
         {
-            new Problem
-            {
-                StorageCapacity = 5,
-                KeepCost = 1,
-                OrderCost = 2,
-                Demands = new int[] { 1, 2, 3, 1 },
-                UnitPrices = new int[] { 1, 2, 1, 2 }
-            }.Solve();
+            Problem p = new Problem();
+
+            Console.WriteLine("Podaj maksymalną pojemność magazynu:");
+            p.StorageCapacity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj koszt złożenia zamówienia:");
+            p.OrderCost = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj koszt okresowego utrzymania jednej sztuki produktu:");
+            p.KeepCost = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Podaj popyt w poszczególnych okresach (oddzielone przecinkami bez spacji):");
+            p.Demands = Array.ConvertAll(Console.ReadLine().Split(','), s => int.Parse(s));
+
+            Console.WriteLine("Podaj cenę jednostkową w poszczególnych okresach (oddzielone przecinkami bez spacji):");
+            p.UnitPrices = Array.ConvertAll(Console.ReadLine().Split(','), s => int.Parse(s));
+
+            Console.WriteLine("\n");
+            
+            p.Solve();
 
             Console.ReadKey();
         }
